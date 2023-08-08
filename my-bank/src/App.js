4181e6390ebaf31dfe4d8e6e1b4c8f39b32a866a
Parth Pant <parthpant4@gmail.com>
2023-08-08 14:22:19 +0530

    add tailwind and daisyUI

--- my-bank/src/App.js
+++ my-bank/src/App.js
@@ -4,9 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Hello World
-      </header>
+      <button class="btn">Login</button>
     </div>
   );
 }
