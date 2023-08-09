fbb52b152cd174361225007fed42e85647a3c53f
Parth Pant <parthpant4@gmail.com>
2023-08-09 21:52:47 +0530

    Add models

---
+++ backend/Models/AccountDto.cs
@@ -0,0 +1,12 @@
+namespace MyBank.API.Models {
+	public class AccountDto {
+        public long AccNo { get; set; }
+        public string AccountType { get; set; } = null!;
+        public long Balance { get; set; }
+        public long CustId { get; set; }
+
+        public AccountDto(string accountType) {
+            AccountType = accountType;
+        }
+	}
+}
---
+++ backend/Models/CustomerDto.cs
@@ -0,0 +1,18 @@
+namespace MyBank.API.Models {
+	public class CustomerDto {
+        public long CustId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public long Contact { get; set; }
+        public long CardNo { get; set; }
+        public long PinNo { get; set; }
+        public string City { get; set; }
+        public ICollection<AccountDto> Accounts {get; set;} = new List<AccountDto>();
+
+        public CustomerDto(string name, string email, string city) {
+            Name = name;
+            Email = email;
+            City = city;
+        }
+	}
+}
