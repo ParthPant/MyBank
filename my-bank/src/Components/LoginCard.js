715ecf53ad717b6a8ab4579dd0dd3b21df398b08
HAJ931 <hananaj910@example.com>
2023-08-10 12:59:01 +0530

    minor code restructuring

--- my-bank/src/Components/LoginCard.js
+++ my-bank/src/Components/LoginCard.js
@@ -5,13 +5,15 @@ function LoginCard() {
   return (
     <div className="card card-compact w-96 bg-base-100 shadow-xl">
         <div className="card-body items-center text-center">
-          <input type="text" placeholder="Username" className="input input-bordered input-primary w-full max-w-xs" />
-          <input type="text" placeholder="Password" className="input input-bordered input-primary w-full max-w-xs" />
-          <div className="card-actions">
-            <Link to ="/admin">
-                <button class="btn">Login</button>
-            </Link>
-          </div>
+            <form>
+            <input type="text" placeholder="Username" className="input input-bordered input-primary w-full max-w-xs" />
+            <input type="text" placeholder="Password" className="input input-bordered input-primary w-full max-w-xs" />
+            <div className="card-actions">
+                <Link to ="/admin">
+                    <button class="btn">Login</button>
+                </Link>
+            </div>
+            </form>
         </div>
       </div>
   );
