96b32b4e0dbb85138370f2031d39bc6e483c9957
HAJ931 <hananaj910@gmail.com>
2023-08-10 10:31:47 +0530

    Created basic login page

--- my-bank/src/Components/LoginCard.js
+++ my-bank/src/Components/LoginCard.js
@@ -1,13 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './Components/Navbar';
-// import {LoginCard} from './Components/LoginCard';
+import React from 'react';
 
-function App() {
+function LoginCard() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="card card-compact w-96 bg-base-100 shadow-xl">
+    <div className="card card-compact w-96 bg-base-100 shadow-xl">
         <div className="card-body items-center text-center">
           <input type="text" placeholder="Username" className="input input-bordered input-primary w-full max-w-xs" />
           <input type="text" placeholder="Password" className="input input-bordered input-primary w-full max-w-xs" />
@@ -16,8 +11,7 @@ function App() {
           </div>
         </div>
       </div>
-    </div>
   );
 }
 
-export default App;
+export default LoginCard;
