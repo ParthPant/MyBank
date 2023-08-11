2fc99361c7650b88f4058fd2e7cbb83c45496fd3
Your Name <you@example.com>
2023-08-11 13:18:12 +0530

    frontend-changes

--- my-bank/src/Components/LoginCard.js
+++ my-bank/src/Components/LoginCard.js
@@ -3,14 +3,14 @@ import {Link} from 'react-router-dom';
 
 function LoginCard() {
   return (
-    <div className="card card-compact w-96 bg-base-100 shadow-xl">
+    <div className="card card-compact w-96 bg-base-200 shadow-xl">
         <div className="card-body items-center text-center">
 
-            <input type="text" placeholder="Username" className="input input-bordered input-primary w-full max-w-xs" />
-            <input type="password" placeholder="Password" className="input input-bordered input-primary w-full max-w-xs" />
+            <input type="text" placeholder="Username" className="input border-black input-bordered input-primary w-full max-w-xs" />
+            <input type="password" placeholder="Password" className="input border-black input-bordered input-primary w-full max-w-xs" />
             <div className="card-actions">
                 <Link to ="/login">
-                    <button class="btn">Login</button>
+                    <button class="btn bg-slate-300">Login</button>
                 </Link>
             </div>
         </div>
