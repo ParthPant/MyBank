fba60df0655b6b95fb95f3fd3b36050ed5d46e8d
HAJ931 <hananaj910@example.com>
2023-08-14 10:48:43 +0530

    Basic Customer Grid

---
+++ my-bank/src/Components/CustomerGrid.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import CustomerDetails from './CustomerDetails';
+
+function CustomerGrid() {
+    let customers = [];
+    for(let i = 0; i <8; ++i) {
+        customers.push({"customerID": "ID "+ i, "name": "Customer " + i})
+    }
+  return (
+    <div class="pl-10 pr-10 grid grid-cols-4 gap-4">
+    {customers.map((customer,index)=>{
+         return <CustomerDetails key={index} customerID={customer.customerID} customerName={customer.name}/>
+     })}
+    </div>
+  );
+}
+
+export default CustomerGrid;
