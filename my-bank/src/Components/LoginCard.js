2c8573af103ed8436edfb795c7dd24d6df7677f5
Mona25-create <you@example.com>
2023-08-16 12:46:21 +0530

    sign up

--- my-bank/src/Components/LoginCard.js
+++ my-bank/src/Components/LoginCard.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 function LoginCard({ onSubmit, error }) {
   const [userName, setUserName] = useState();
   const [password, setPassword] = useState();
+  const [isSignIn, setisSignin] = useState(true);
 
   return (
     <div className="card w-96 bg-gray-900">
@@ -21,21 +22,40 @@ function LoginCard({ onSubmit, error }) {
           className="input border-black input-bordered input-primary w-full max-w-xs"
           onChange={(e) => setPassword(e.target.value)}
         />
-        {error ? (
+        { !isSignIn ?  <input
+          type="password"
+          placeholder="Confirm Password"
+          className="input border-black input-bordered input-primary w-full max-w-xs"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        : <></>}
+               {error ? (
           <div><b className="text-red-600">Wrong user credentials provided</b></div>
         ) : (
           <></>
         )}
-        <div className="card-actions">
+        { isSignIn ? <div className="card-actions">
           <button
             class="btn btn-primary bg-purple-600 rounded-none"
             onClick={() => onSubmit(userName, password)}
           >
             Login
           </button>
-        </div>
+        </div> : <></>}
+        { !isSignIn ? <div className="card-actions">
+          <button
+            class="btn btn-primary bg-purple-600 rounded-none"
+            onClick={() => onSubmit(userName, password)}
+          >
+            Sign up
+          </button>
+        </div> : <></>}
+        { !isSignIn ? <div onClick={()=> {setisSignin(!isSignIn)}}class="text-blue-400 hover:underline">Already have an account? Log in</div> : <></>}
+ 
+        { isSignIn ? <div onClick={()=> {setisSignin(!isSignIn)}}class="text-blue-400 hover:underline">New user? Create new account</div> : <></>}
       </div>
     </div>
+
   );
 }
 
