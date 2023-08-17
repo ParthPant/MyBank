893a896d8e7be413e7e89596cb63e45c4bdea6a7
HAJ931 <hananaj910@example.com>
2023-08-17 10:01:14 +0530

    Added footer

---
+++ my-bank/src/Components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Footer() {
+    return (
+        <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+        <div>
+            <p>Copyright © 2023 - All rights reserved by Wells Fargo</p>
+        </div>
+        </footer>
+    );
+}
